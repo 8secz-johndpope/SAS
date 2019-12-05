f66b35919313330e07e7ce92e897bed7da7fb2a1
Lauren Saxton <lnsaxton@udel.edu>
2019-12-04 23:24:32 -0600

    placing rocket parts

--- SAS/CameraView1.swift
+++ SAS/CameraView1.swift
@@ -36,20 +36,45 @@ class CameraView1: UIViewController, ARSCNViewDelegate {
          //for adding multiple scns
          // https://stackoverflow.com/questions/28655395/scenekit-display-multiple-scenes-in-same-scnview
          
-         let subScene = SCNScene(named: "art.scnassets/rwing.scn")!
-         let rwing = subScene.rootNode.childNode(withName: "shipMesh", recursively: true)!
+         let rw_subScene = SCNScene(named: "art.scnassets/rwing.scn")!
+         let rwing = rw_subScene.rootNode.childNode(withName: "shipMesh", recursively: true)!
          rwing.name = "rwing"
+        
+         let lw_subScene = SCNScene(named: "art.scnassets/lwing.scn")!
+         let lwing = lw_subScene.rootNode.childNode(withName: "shipMesh", recursively: true)!
+         lwing.name = "lwing"
          
+         let rf_subScene = SCNScene(named: "art.scnassets/rfin.scn")!
+         let rfin = rf_subScene.rootNode.childNode(withName: "shipMesh", recursively: true)!
+         rfin.name = "rfin"
+        
+         let lf_subScene = SCNScene(named: "art.scnassets/lfin.scn")!
+         let lfin = lf_subScene.rootNode.childNode(withName: "shipMesh", recursively: true)!
+         lfin.name = "lfin"
+        
          //BEGIN code with citation - how to position things code
          // https://blog.markdaws.net/arkit-by-example-part1-7830677ef84d
-         rwing.position = SCNVector3Make(0, 0, -5.0);
-        
+         rwing.position = SCNVector3Make(0.3, 0.1, -1.0);
+         lwing.position = SCNVector3Make(0.3, 0.1, -1.0);
+         rfin.position = SCNVector3Make(0, 0.2, -1.5);
+         lfin.position = SCNVector3Make(0.2, 0.2, -1.5);
          //END CODE with citation - how to position things code
-         
+        
          sceneView.scene.rootNode.addChildNode(rwing)
+         sceneView.scene.rootNode.addChildNode(lwing)
+         sceneView.scene.rootNode.addChildNode(rfin)
+         sceneView.scene.rootNode.addChildNode(lfin)
          //END CODE with citation - multiple scenes
-
+        
+         //BEGIN code with citation - how to rotate objects
+         //https://stackoverflow.com/questions/50368377/rotate-object-in-arkit
+         lfin.eulerAngles.x = 90
+         rfin.eulerAngles.x = 90
+         lwing.eulerAngles.x = -99
+         rwing.eulerAngles.x = -99
+         // END code - how to rotate objects
      }
+    
      override func viewWillAppear(_ animated: Bool) {
          super.viewWillAppear(animated)
          sceneView.debugOptions = ARSCNDebugOptions.showFeaturePoints
@@ -57,6 +82,7 @@ class CameraView1: UIViewController, ARSCNViewDelegate {
          configuration.planeDetection = .horizontal
          sceneView.session.run(configuration)
      }
+    
  }
 //END CODE with citation - basic setup
 
