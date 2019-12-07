a5ca72951733d25c101a738803e016916ad0453a
Lauren Saxton <lnsaxton@udel.edu>
2019-12-06 22:07:26 -0600

    adding done button

--- SAS/CameraView1.swift
+++ SAS/CameraView1.swift
@@ -21,6 +21,10 @@ class CameraView1: UIViewController, ARSCNViewDelegate {
 
 var empty_node = SCNNode()
 var current_node = SCNNode()
+var rwing = SCNNode()
+var lwing = SCNNode()
+var rfin = SCNNode()
+var lfin = SCNNode()
 
 //BEGIN code with citation - text node
 // https://stackoverflow.com/questions/35733916/changing-the-value-of-string-on-scntext-produces-no-change
@@ -49,17 +53,16 @@ var textGeometry = SCNText(string: "Target is None", extrusionDepth: 1)
          // https://stackoverflow.com/questions/28655395/scenekit-display-multiple-scenes-in-same-scnview
          
          let rw_subScene = SCNScene(named: "art.scnassets/rwing.scn")!
-         let rwing = rw_subScene.rootNode.childNode(withName: "rwing", recursively: true)!
+         rwing = rw_subScene.rootNode.childNode(withName: "rwing", recursively: true)!
         
          let lw_subScene = SCNScene(named: "art.scnassets/lwing.scn")!
-         let lwing = lw_subScene.rootNode.childNode(withName: "lwing", recursively: true)!
+         lwing = lw_subScene.rootNode.childNode(withName: "lwing", recursively: true)!
          
          let rf_subScene = SCNScene(named: "art.scnassets/rfin.scn")!
-         let rfin = rf_subScene.rootNode.childNode(withName: "rfin", recursively: true)!
+         rfin = rf_subScene.rootNode.childNode(withName: "rfin", recursively: true)!
         
          let lf_subScene = SCNScene(named: "art.scnassets/lfin.scn")!
-         let lfin = lf_subScene.rootNode.childNode(withName: "lfin", recursively: true)!
-         lfin.name = "lfin"
+         lfin = lf_subScene.rootNode.childNode(withName: "lfin", recursively: true)!
         
          //BEGIN code with citation - naming nodes
          // https://mobile-ar.reality.news/how-to/arkit-101-pilot-your-3d-plane-location-using-hittest-arkit-0184060/
@@ -97,7 +100,7 @@ var textGeometry = SCNText(string: "Target is None", extrusionDepth: 1)
          // https://stackoverflow.com/questions/50591866/how-to-point-and-position-arkit-text
          textGeometry.font = UIFont(name: "Helvatica", size: 1)
          textGeometry.flatness = 0
-         textGeometry.firstMaterial?.diffuse.contents = UIColor.blue
+         textGeometry.firstMaterial?.diffuse.contents = UIColor.white
          textNode.geometry = textGeometry
          let min = textNode.boundingBox.min
          let max = textNode.boundingBox.max
@@ -108,7 +111,25 @@ var textGeometry = SCNText(string: "Target is None", extrusionDepth: 1)
          )
          textNode.scale = SCNVector3(0.005, 0.005 , 0.005)
          sceneView.scene.rootNode.addChildNode(textNode)
-         textNode.position = SCNVector3Make(0.2, 0.2, -2.0);
+         textNode.position = SCNVector3Make(0.5, -1.0, -2.0);
+        
+         let textNode2 = SCNNode()
+         let textGeometry2 = SCNText(string: "I'm done!", extrusionDepth: 1)
+         textGeometry2.font = UIFont(name: "Helvatica", size: 1)
+         textGeometry2.flatness = 0
+         textGeometry2.firstMaterial?.diffuse.contents = UIColor.red
+         textNode2.geometry = textGeometry2
+         let min2 = textNode2.boundingBox.min
+         let max2 = textNode2.boundingBox.max
+         textNode.pivot = SCNMatrix4MakeTranslation(
+             min2.x + (max2.x - min2.x)/2,
+             min2.y + (max2.y - min2.y)/2,
+             min2.z + (max2.z - min2.z)/2
+         )
+         textNode2.scale = SCNVector3(0.005, 0.005 , 0.005)
+         textNode2.name = "done"
+         sceneView.scene.rootNode.addChildNode(textNode2)
+         textNode2.position = SCNVector3Make(-0.5, 0.5, -1.0);
         //END code with citation - text node
      }
     
@@ -155,11 +176,28 @@ var textGeometry = SCNText(string: "Target is None", extrusionDepth: 1)
             textGeometry = SCNText(string: "Target is " + (result.node.name ?? "None") ?? "None", extrusionDepth: 1)
             textNode.geometry = textGeometry
             break
+        } else if name == "done" {
+            animateAll()
         }
+        
     }
     //END code with citation - for each loops
-     
  }
+    
+    private func animateAll() {
+        let rwing_position = SCNVector3Make(0.1, -0.1, -0.8);
+        let lwing_position = SCNVector3Make(0, -0.1, -0.8);
+        let rfin_position = SCNVector3Make(0, -0.3, -0.8);
+        let lfin_position = SCNVector3Make(0, -0.3, -0.8);
+        let rwing_action = SCNAction.move(to: rwing_position, duration: 5)
+        let lwing_action = SCNAction.move(to: lwing_position, duration: 5)
+        let rfin_action = SCNAction.move(to: rfin_position, duration: 5)
+        let lfin_action = SCNAction.move(to: lfin_position, duration: 5)
+        rwing.runAction(rwing_action)
+        lwing.runAction(lwing_action)
+        rfin.runAction(rfin_action)
+        lfin.runAction(lfin_action)
+    }
     
  @objc func doubleTapped(recognizer: UIGestureRecognizer) {
      // Get exact position where touch happened on screen of iPhone (2D coordinate)
